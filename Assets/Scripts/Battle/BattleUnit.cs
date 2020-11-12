bcae8bac3aa8720d23d3247ae535abec0de745eb
511259 <511259@chrome.escg.ac.uk>
2020-11-12 10:32:24 +0000

    Added Enter Animation

--- Assets/Scripts/Battle/BattleUnit.cs
+++ Assets/Scripts/Battle/BattleUnit.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class BattleUnit : MonoBehaviour
 {
@@ -11,12 +12,33 @@ public class BattleUnit : MonoBehaviour
 
     public Pokemon Pokemon { get; set; }
 
+    Image image;
+    Vector3 originalPos;
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        originalPos = image.transform.localPosition;
+    }
+
     public void Setup()
     {
        Pokemon = new Pokemon(_base, level);
         if (isPlayerUnit)
-            GetComponent<Image>().sprite = Pokemon.Base.BackSprite;
+            image.sprite = Pokemon.Base.BackSprite;
         else
-            GetComponent<Image>().sprite = Pokemon.Base.FrontSprite;
+            image.sprite = Pokemon.Base.FrontSprite;
+
+        PlayerEnterAnimation();
+    }
+
+  
+    public void PlayerEnterAnimation()
+    {
+        if (isPlayerUnit)
+            image.transform.localPosition = new Vector3(-500f, originalPos.y);
+        else
+            image.transform.localPosition = new Vector3(500f, originalPos.y);
+
+        image.transform.DOLocalMoveX(originalPos.x, 1f);
     }
 }
